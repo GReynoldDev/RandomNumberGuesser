ff15aa76faf283b2eeb87606c98125eed94b38b1
GReynoldDev <GReynoldDev@gmail.com>
2024-03-23 19:34:45 -0700

    Completed RandomNumberGuesser.py

--- RandomNumberGuesser.py
+++ RandomNumberGuesser.py
@@ -1,2 +1,14 @@
-print("Hello World!")
-print("Hello World 2")
+import random
+
+secret_number = random.randint(1, 100)
+
+while True:
+    guess = int(input("Enter your guess (1,100): "))
+            
+    if guess == secret_number:
+        print("Congratulations! you guessed the number.")
+        break
+    elif guess < secret_number:
+        print("Too low. Try again.")
+    else:
+        print("Too high. Try again.")
